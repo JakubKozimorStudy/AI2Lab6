a8f9418465075039db876bebcee06ae68755e0a1
Jakub <gudzikoff@gmail.com>
2019-12-08 02:24:33 +0100

    Manipulating

--- venv/Include/crossworld.py
+++ venv/Include/crossworld.py
@@ -1,25 +1,13 @@
-import string
-import re
-def regex_crossword_check(horizontal_patterns, vertical_patterns, width, height, candidate, alphabet=string.ascii_uppercase):
-    for pattern, horiz_line in zip(horizontal_patterns, candidate):
-        line = ''.join(horiz_line)
-        if re.match(pattern, line) is None:
-            return False
+import itertools
 
-    for pattern, vert_line in zip(vertical_patterns, zip(*candidate)):
-        line = ''.join(vert_line)
-        if re.match(pattern, line) is None:
-            return False
-
-    return True
+def tabulate(f, start=0, step=0):
+	return map(f, itertools.count())
 
 
 
-horiz = [r'HE|LL|O+', r'[PLEASE]+']
-vert = [r'[^SPEAK]+', r'EP|IP|EF']
-candidate = [
-	['H', 'E'],
-	['L', 'P']
-]
-print(regex_crossword_check(horiz, vert,vert.__sizeof__(),horiz.__sizeof__(), candidate))  # => True
-
+sqgen = tabulate(lambda x: x ** 2)
+print(next(sqgen)) # => 0
+print(next(sqgen)) # => 1
+print(next(sqgen)) # => 2
+print(next(sqgen)) # => 4
+print(next(sqgen)) # => 9
